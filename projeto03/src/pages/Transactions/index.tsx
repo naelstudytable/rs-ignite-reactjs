d96df24f7cd9448781bd95cbeae656709ef496bd
Natanael A. de Morais <natanaelallves@gmail.com>
2023-02-20 16:24:06 -0300

    build(projeto03|Estrutura visual): Tabela transações

--- projeto03/src/pages/Transactions/index.tsx
+++ projeto03/src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionContainer, TransactionTable } from "./style";
 
 
 export function Transactions() {
@@ -8,6 +9,33 @@ export function Transactions() {
             <Header />
 
             <Summary />
+
+            <TransactionContainer>
+                <TransactionTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Des1</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Des2</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    - R$ 44,88
+                                </PriceHighLight>
+                            </td>
+                            <td>Campra</td>
+                            <td>30/05/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionTable>
+            </TransactionContainer>
         </div>
     )
 }
