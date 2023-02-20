f4ca693802e5cda6f127f3a411b699f0bdd50e75
Natanael A. de Morais <natanaelallves@gmail.com>
2023-02-20 11:32:28 -0300

    build(projeto03|Estrutura visual): Componente: Header

--- projeto03/src/App.tsx
+++ projeto03/src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -8,7 +9,8 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Bom dia!</h1>
+
+      <Transactions />
     </ThemeProvider>
   )
 }
---
+++ projeto03/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
+
+import logoImg from "../../assets/logo.svg"
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoImg} />
+
+                <NewTransactionButton>
+                    Nova transação
+                </NewTransactionButton>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
