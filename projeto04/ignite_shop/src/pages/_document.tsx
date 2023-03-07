da5439a3bbf035fc4aa82d6f4e76279c196cd9fe
Natanael A. de Morais <natanael.tocourses@gmail.com>
2023-03-06 21:59:02 -0300

    build(projeto04|Estrutura visual): Configurando Stitches

--- projeto04/ignite_shop/src/pages/_document.tsx
+++ projeto04/ignite_shop/src/pages/_document.tsx
@@ -1,13 +1,14 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { getCssText } from "../styles";
 
-
-export function Document() {
+export default function Document() {
     return (
         <Html> {/* <html></html> */}
             <Head> {/* <head></head> */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+                <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
             </Head>
 
             <body>
---
+++ projeto04/ignite_shop/src/styles/index.ts
@@ -0,0 +1,19 @@
+
+import { createStitches } from "@stitches/react";
+
+export const {
+    config,
+    styled,
+    css,
+    globalCss,
+    keyframes,
+    getCssText,
+    theme,
+    createTheme,
+} = createStitches({
+    theme: {
+        colors: {
+            nael: '#8257e6',
+        }
+    }
+})
