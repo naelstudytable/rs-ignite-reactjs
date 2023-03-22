a0ab13a74116a2dca300b13f5c2b8b9555055d58
Natanael A. de Morais <natanael.tocourses@gmail.com>
2023-03-21 21:38:18 -0300

    build(projeto04|Produto & Checkout): Fallback do SSG

--- projeto04/ignite_shop/src/pages/product/[id].tsx
+++ projeto04/ignite_shop/src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from "next"
 import Image from "next/image";
+import { useRouter } from "next/router";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
@@ -15,6 +16,12 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+    const { isFallback } = useRouter()
+
+    if (isFallback) {
+        return <p>Loading...</p>
+    }
+
     return (
         <ProductContainer>
             <ImageContainer>
@@ -39,10 +46,10 @@ export const getStaticPaths: GetStaticPaths = async () => {
     return {
         paths: [
             {
-                params: { id: 'prod_MLH5Wy0Y97hDAC' }
+                params: { id: 'prod_NYU0XZD5NjNoqS' }
             }
         ],
-        fallback: false,
+        fallback: true,
     }
 }
 
