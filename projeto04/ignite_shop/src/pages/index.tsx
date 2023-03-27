d4a14c6afc1efe95925947d775b6e6c6b6a56e7d
Natanael A. de Morais <natanael.tocourses@gmail.com>
2023-03-27 20:38:03 -0300

    build(projeto04|Produto & Checkout): Trabalhando com SEO

--- projeto04/ignite_shop/src/pages/index.tsx
+++ projeto04/ignite_shop/src/pages/index.tsx
@@ -8,6 +8,7 @@ import { stripe } from "../lib/stripe"
 import { HomeContainer, Product } from "../styles/pages/home"
 import 'keen-slider/keen-slider.min.css'
 import Stripe from "stripe"
+import Head from "next/head"
 interface HomeProps {
   products: {
     id: string
@@ -24,24 +25,30 @@ export default function Home({ products }: HomeProps) {
     }
   });
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map(product => {
-        return (
-          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
-            <Product
-              className="keen-slider__slide"
-            >
-              <Image src={product.imageUrl} width={520} height={480} alt="" />
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
 
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
-          </Link>
-        )
-      })}
-    </HomeContainer>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map(product => {
+          return (
+            <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+              <Product
+                className="keen-slider__slide"
+              >
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
 export const getStaticProps: GetStaticProps = async () => {
